33bb810580446889e196caa785e1184da9641b8f
Red74Moon <fedoormaster@gmail.com>
2021-09-29 11:38:54 +0900

    2차원 배열 MAP

--- main.cpp
+++ main.cpp
@@ -1,39 +1,27 @@
+
 #include <iostream>
-#include <ctime>
 
 using namespace std;
 
 int main()
 {
-	//배열 선언
-	int Bingo[9];
-	//배열 초기화
-	for (int i = 0; i < 9; ++i)
+	char Map[10][10] = {
+		{1,1,1,1,1,1,1,1,1,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,1,1,1,1,1,1,1,1,1}
+	};
+	for (int Y = 0; Y < 10; ++Y)
 	{
-		Bingo[i] = i + 1;
-	}
-	//난수 발생
-	srand(static_cast<unsigned int>(time(nullptr)));
-	//배열 자리바꿈
-	for (int i = 0; i < 100; ++i)
-	{
-		//9개 자리수중 무작위 위치 2개 추출
-		int index1 = rand() % 9;
-		int index2 = rand() % 9;
-		//무작위 추출된 2개의 값 바꿈
-		int Temp	  = Bingo[index1];
-		Bingo[index1] = Bingo[index2];
-		Bingo[index2] = Temp;
-	}
-	//배열 출력 3*3
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int j = 0; j < 3; ++j)
+		for (int X = 0; X < 10; ++X)
 		{
-			if (Bingo[i * 3 + j] == INT_MAX)
-				cout << "*\t";
-			else
-				cout << Bingo[i * 3 + j] << "\t";
+			cout << static_cast<int>(Map[X][Y])<< " ";
 		}
 		cout << endl;
 	}
